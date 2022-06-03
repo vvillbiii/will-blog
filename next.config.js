59c4da4836430b189946a894499f33e344602839
vvillbii <will.bowles90@gmail.com>
2022-06-03 14:28:19 -0400

    added i18n to next.config to set lang to eng

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  //html lang eng tag
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
