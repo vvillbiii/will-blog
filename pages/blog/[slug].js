cd659b0520ae94d644ade69a9c66dfbe4f122030
vvillbii <will.bowles90@gmail.com>
2022-10-06 07:44:38 -0400

    updated blog detail canoncial url

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Layout from "../../components/Layout";
 import * as contentful from "contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
 const client = contentful.createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -11,7 +11,7 @@ const client = contentful.createClient({
 
 const BlogDetails = ({ blogs }) => {
   const site = "https://willbowles.co/blog";
-  const canonicalURL = site + useRouter().pathname;
+  const canonicalURL = site + blogs.fields.slug;
   return (
     <Layout>
       <Head>
